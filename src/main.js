40f46d486ca7c51f2677d12387fc0b2a694ddd64
shenjindui <2802630961@qq.com>
2020-05-06 11:59:38 +0800

    #代码注释优化

--- src/main.js
+++ src/main.js
@@ -4,9 +4,8 @@ import router from './router'
 import axios from 'axios';
 import layer from 'vue-layer'
 import ElementUI from 'element-ui';
-//引入表单验证组件
 import 'element-ui/lib/theme-chalk/index.css'; // 默认主题
-// import '../static/css/theme-green/index.css';       // 浅绿色主题
+// import '../static/css/theme-green/index.css';// 浅绿色主题
 import './assets/css/icon.css';
 /*import './components/common/directives';*/
 import "babel-polyfill";
@@ -14,7 +13,9 @@ import SlideVerify from 'vue-monoplasty-slide-verify';
 import Loading  from 'element-ui';
 Vue.use(Loading)
 Vue.use(SlideVerify);
-//将引入的组件注册为全局Vue组件
+/**
+ * 将引入的组件注册为全局Vue组件
+ */
 Vue.use(ElementUI);
 Vue.prototype.$layer = layer(Vue)
 Vue.prototype.$axios = axios;
@@ -22,43 +23,38 @@ Vue.prototype.$axios = axios;
  * Vue.use(Button)
  * Vue.use(Select)
  */
-
 Vue.config.productionTip = false
 Vue.use(ElementUI, {
     size: 'small'
 });
-
-//在main.js设置全局的请求次数，请求的间隙
+/**
+ * main.js设置全局的请求次数，请求的间隙
+ * @type {number}
+ */
 axios.defaults.retry = 2;//设置请求失败重新请求的次数
 axios.defaults.retryDelay = 1000;
 axios.interceptors.response.use(undefined, function axiosRetryInterceptor(err) {
     var config = err.config;
-    // If config does not exist or the retry option is not set, reject
     if(!config || !config.retry) return Promise.reject(err);
-    // Set the variable for keeping track of the retry count
     config.__retryCount = config.__retryCount || 0;
-    // Check if we've maxed out the total number of retries
     if(config.__retryCount >= config.retry) {
-        // Reject with the error
         return Promise.reject(err);
     }
-    // Increase the retry count
     config.__retryCount += 1;
-    // Create new promise to handle exponential backoff
     var backoff = new Promise(function(resolve) {
         setTimeout(function() {
             resolve();
         }, config.retryDelay || 1);
     });
-    // Return the promise in which recalls axios to retry the request
     return backoff.then(function() {
         return axios(config);
     });
 });
-//后端访问的路径
+/**
+ * 后端访问的路径
+ * @type {string}
+ */
 axios.default.baseURL = 'http://localhost:8889'
-
-
 //使用钩子函数对路由进行权限跳转
 router.beforeEach((to, from, next) => {
     //这边可以用match()来判断所有需要权限的路径，to.matched.some(item => return item.meta.loginRequire)
@@ -79,8 +75,6 @@ router.beforeEach((to, from, next) => {
         }
     }*/
 })
-
-
 new Vue({
     router,
     render: h => h(App)

50b6ae220d633fbe3db937c99da6474280921f3c
shenjindui <2802630961@qq.com>
2020-05-06 11:56:57 +0800

    #代码注释优化

--- vue.config.js
+++ vue.config.js
@@ -1,24 +1,51 @@
+/**
+ * author shenjindui 2019.12.26
+ * vue配置文件
+ * @type {{devServer: {proxy: {"/ms": {changeOrigin: boolean, target: string}, "/api": {changeOrigin: boolean, pathRewrite: {"^/api": string}, target: string}}}, outputDir: string, productionSourceMap: boolean, baseUrl: string, css: {loaderOptions: {sass: {data: string}}, extract: boolean, sourceMap: boolean, modules: boolean}, pages: undefined, parallel: boolean, configureWebpack: module.exports.configureWebpack, assetsDir: string, runtimeCompiler: boolean, lintOnSave: boolean, chainWebpack: module.exports.chainWebpack}}
+ */
 module.exports = {
-    lintOnSave:false,//关闭eslint规范
-    //基本路径
-    baseUrl: '/',//vue-cli3.3以下版本使用
-   // publicPath:'./',//vue-cli3.3+新版本使用
-    //输出文件目录
+    /**
+     * 关闭eslint规范
+     */
+    lintOnSave:false,
+    /**
+     * 基本路径
+     */
+    baseUrl: '/',
+    /**
+     * 输出文件目录
+     */
     outputDir: 'dist',
-    // eslint-loader 是否在保存的时候检查
+    /**
+     * eslint-loader 是否在保存的时候检查
+     */
     lintOnSave: true,
-    //放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+    /**
+     * 放置生成的静态资源 (js、css、img、fonts) 的 (相对于 outputDir 的) 目录。
+     */
     assetsDir: 'static',
-    //以多页模式构建应用程序。
+    /**
+     * 以多页模式构建应用程序。
+     */
     pages: undefined,
-    //是否使用包含运行时编译器的 Vue 构建版本
+    /**
+     * 是否使用包含运行时编译器的 Vue 构建版本
+     */
     runtimeCompiler: false,
-    //是否为 Babel 或 TypeScript 使用 thread-loader。该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建，在适当的时候开启几个子进程去并发的执行压缩
+    /**
+     * 是否为 Babel 或 TypeScript 使用 thread-loader。
+     * 该选项在系统的 CPU 有多于一个内核时自动启用，仅作用于生产构建，
+     * 在适当的时候开启几个子进程去并发的执行压缩
+     */
     parallel: require('os').cpus().length > 1,
-    //生产环境是否生成 sourceMap 文件，一般情况不建议打开
+    /**
+     * 生产环境是否生成 sourceMap 文件，一般情况不打开
+     */
     productionSourceMap: false,
-    // webpack配置
-    //对内部的 webpack 配置进行更细粒度的修改 https://github.com/neutrinojs/webpack-chain see https://github.com/vuejs/vue-cli/blob/dev/docs/webpack.md
+    /**
+     * webpack配置
+     * @param config
+     */
     chainWebpack: config => {
         /**
          * 删除懒加载模块的prefetch，降低带宽压力
@@ -30,9 +57,9 @@ module.exports = {
         //} else {// 为开发环境修改配置...
         //}
     },
-    //调整 webpack 配置 https://cli.vuejs.org/zh/guide/webpack.html#%E7%AE%80%E5%8D%95%E7%9A%84%E9%85%8D%E7%BD%AE%E6%96%B9%E5%BC%8F
+    //调整 webpack 配置
     configureWebpack: config => {
-        //生产and测试环境
+        //生产与测试环境
         /*let pluginsPro = [
             new CompressionPlugin({ //文件开启Gzip，也可以通过服务端(如：nginx)(https://github.com/webpack-contrib/compression-webpack-plugin)
                 filename: '[path].gz[query]',
@@ -60,13 +87,21 @@ module.exports = {
         }*/
     },
     css: {
-        // 启用 CSS modules
+        /**
+         * 启用 CSS modules
+         */
         modules: false,
-        // 是否使用css分离插件
+        /**
+         * 是否使用css分离插件
+         */
         extract: true,
-        // 开启 CSS source maps，一般不建议开启
+        /**
+         * 开启 CSS source maps，一般不开启
+         */
         sourceMap: false,
-        // css预设器配置项
+        /**
+         * css预设器配置项
+         */
         loaderOptions: {
             sass: {
                 //设置css中引用文件的路径，引入通用使用的scss文件（如包含的@mixin）
@@ -80,8 +115,6 @@ module.exports = {
             }
         }
     },
-   /// baseUrl: './',
-    //productionSourceMap: false,
     devServer: {
         proxy: {
             '/api':{
